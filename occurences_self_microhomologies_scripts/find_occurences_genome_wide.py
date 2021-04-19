002e71ba3106835bebc70ea65921a74c6f0e829f
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-04-19 10:54:22 +0200

    Correction d'indentations et de lower() cases

--- occurences_self_microhomologies_scripts/find_occurences_genome_wide.py
+++ occurences_self_microhomologies_scripts/find_occurences_genome_wide.py
@@ -9,19 +9,20 @@ from Bio.SeqIO.FastaIO import FastaIterator
 def frequency_genome_wide (fragment, genome_ref):
     total = []
     for micro in fragment:
-        count = 0
-        genome =  open(genome_ref, "r")
-
-        for record in FastaIterator(genome):
-            seq = str(record.seq).lower()
-            revcomp_sep = str(record.reverse_complement().seq).lower()
-
-            count += len(re.findall(pattern=micro, string=seq))
-            count += len(re.findall(pattern=micro, string=revcomp_sep))
-        genome.close()
-
-        total.append(count)
-        
+    	micro = micro.lower()
+    	count = 0
+    	genome =  open(genome_ref, "r")
+    	
+    	for record in FastaIterator(genome):
+    		seq = str(record.seq).lower()
+    		revcomp_sep = str(record.reverse_complement().seq).lower()
+    		
+    		count += len(re.findall(pattern=micro, string=seq))
+    		count += len(re.findall(pattern=micro, string=revcomp_sep))
+    		
+    	genome.close()
+    	total.append(count)
+    	
     return(total)
     
     
@@ -31,7 +32,7 @@ def find_microhomologies_8bp (seq, genome_ref):
     start = []
     for i in range(7, len(seq), 1):
         start.append(seq[i-7].upper())
-        sequences.append(seq[i-7:i+1])
+        sequences.append(seq[i-7:i+1].lower())
     
     total = frequency_genome_wide (sequences, genome_ref)
     df = pd.DataFrame({'start' : start, 'sequences' : sequences, "total" : total}).astype(
@@ -47,8 +48,7 @@ def find_microhomologies_8bp (seq, genome_ref):
 if __name__ == "__main__" : 
 
 	fragment = input("Please input your nucleotides sequence fragment here : \n")
-	genome = input("Please indicate the path to the genome_wide FASTA sequence : \n")
-	#../yeast-genome/S288c-R64-2-1-v2014/Genome_S288c.fa
+	genome = "../yeast-genome/S288c-R64-2-1-v2014/Genome_S288c.fa"
 	 
 	df = find_microhomologies_8bp(fragment, genome)
 	if not os.path.exists('./output_files'):
@@ -56,11 +56,11 @@ if __name__ == "__main__" :
 
 	file2save = input("Please enter the name for the output file that will contain the occurences in genome wide : \n")
 	
-	file = open('./output_files/' + str(file2save), 'w')
+	file = open('./output_files/' + str(file2save) + "_occurences_per_8bp_(for_rev_donor).txt", 'w')
 	file.write(df.to_string())
 	file.close()
 	
 	
-	os.system("cat ./output_files/" + str(file2save))
-	print("\n --- DONE --- \n")
+	os.system("cat ./output_files/" + str(file2save) + "_occurences_per_8bp_(for_rev_donor).txt")
+	print("\n\n --- DONE --- \n")
 
--- occurences_self_microhomologies_scripts/find_self_microhomologies.py
+++ occurences_self_microhomologies_scripts/find_self_microhomologies.py
@@ -10,7 +10,7 @@ def make_8bp_micros (seq):
     start = []
     for i in range(7, len(seq), 1):
         start.append(seq[i-7].upper())
-        sequences.append(seq[i-7:i+1])
+        sequences.append(seq[i-7:i+1].lower())
     
     df = pd.DataFrame({'start' : start, 'sequences' : sequences}).astype(
         {'start' : str, 'sequences' : str})
@@ -22,17 +22,17 @@ def self_microhomologies (seq):
     
     self_micros = {}
     for micro in make_8bp_micros(seq)['sequences'].values:
-        pos = {}
-        res = [s.start()+1 for s in re.finditer(pattern=micro, string=seq)]
-        
-        if(len(res) >1):
+    	micro = micro.lower()
+    	pos = {}
+    	res = [s.start()+1 for s in re.finditer(pattern=micro, string=seq.lower())]
+    	
+    	if(len(res) >1):
             for i in range(len(res)):
                 pos["position"+str(i+1)] = pos.get("position"+str(i+1), res[i])
 
             self_micros[micro] = self_micros.get(micro, pos)
             
-        df = pd.DataFrame(self_micros).transpose()
-        
+    df = pd.DataFrame(self_micros).transpose()        
     return(df)
     
 #######################################################
@@ -44,10 +44,10 @@ if __name__ == "__main__" :
 	
 	file2save = input("Please enter the name for the output file that will contain the self occurences : \n")
 	
-	file = open('./output_files/' + str(file2save), 'w')
+	file = open('./output_files/' + str(file2save) + "self_micros.txt", 'w')
 	file.write(df.to_string())
 	file.close()
 	
 	
-	os.system("cat ./output_files/" + str(file2save))
+	os.system("cat ./output_files/" + str(file2save) + "self_micros.txt")
 	print("\n\n --- DONE --- \n")
