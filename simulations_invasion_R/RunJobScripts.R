44d346a93b09aae9ff9735659b62c830546457d3
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-04-28 11:24:59 +0200

    Ajout librairie parallel + commentaire sur le nombre de CPUs à utiliser

--- simulations_invasion_R/RunJobScripts.R
+++ simulations_invasion_R/RunJobScripts.R
@@ -4,6 +4,17 @@
 #     remotes::install_github("rtsudio/rstudioapi")
 
 library(rstudioapi)
+library(parallel)
 
 ###Set working directory :
 setwd("/home/nicolas/Documents/INSA/Stage4BiM/DSB_homologous_recombination_simulation/")
+
+###Detect how many CPU you have 
+### You can run as many simulations as you have CPUs ;
+### But it's recommended to not use all the CPUs, keep at least 2 or 3 free CPUs ;
+
+
+parallel::detectCores()
+
+
+
