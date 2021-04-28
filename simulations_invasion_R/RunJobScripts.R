6df723bd51ce84b1d2b64c86354239de058c9c48
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-04-28 11:20:16 +0200

    Script pour run plusieurs en simultanée

---
+++ simulations_invasion_R/RunJobScripts.R
@@ -0,0 +1,9 @@
+
+# If not installed :
+#     install.packages("remotes")
+#     remotes::install_github("rtsudio/rstudioapi")
+
+library(rstudioapi)
+
+###Set working directory :
+setwd("/home/nicolas/Documents/INSA/Stage4BiM/DSB_homologous_recombination_simulation/")
