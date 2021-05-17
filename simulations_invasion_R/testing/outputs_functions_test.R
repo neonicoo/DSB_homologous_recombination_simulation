f5cecee9e13b6dc11d842ce0823dcd20bed0f030
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-05-17 15:38:55 +0200

    Modification fonction population.time.series()

--- simulations_invasion_R/testing/outputs_functions_test.R
+++ simulations_invasion_R/testing/outputs_functions_test.R
@@ -29,53 +29,30 @@ single.runs <-function(dirnew_singles, ly.binding.ts){
 #########################################################################################################
 #########################################################################################################
 
-population.time.series <- function(dirnew_plots, donors.list, pop.time.series.all.zip, pop.time.series.all.homo, 
-                                   pop.time.series.lys.zip, pop.time.series.lys.homo){
+
+population.time.series <- function(dirnew_plots, donors.list, pop.time.series){
   
-  if(length(donors.list$id)>1){
+  for (i in 1:length(donors.list$id)){
+    df <- subset(x=pop.time.series, select=c(1, 2, i+2))
+    colnames(df) = c("time.step", "length", "prob.detect")
     
-    write.table(pop.time.series.all.zip, file=paste(dirnew_data,"/population_time_series_all_zip.txt",sep=""))
-    outname=paste(dirnew_plots,"/population_time_series_all_zip.png",sep="")
+    df.name <- paste("pop_timeseries", as.character(donors.list$id[i]),as.character(donors.list$bins[i]), sep = "_")
+
+    write.table(df, file=paste(dirnew_data, "/", df.name ,".txt", sep=""))
+    outname=paste(dirnew_plots, "/", df.name, ".png",sep="")
+    
     pop.plot<-
-      ggplot(data = pop.time.series.all.zip) + geom_step(aes(x = time.step, y = prob.detect, color = length)) +
+      ggplot(data = df) + geom_step(aes(x = time.step, y = prob.detect, color = length)) +
       labs(x = "time step", y = "Probability of Detection") + theme_minimal()+ theme(text = element_text(size = 16))+
-      scale_y_continuous(limits = c(0, max(pop.time.series.all.zip$prob.detect)+1))
-    ggsave(outname,plot=pop.plot)
-    
-    
-    write.table(pop.time.series.lys.homo, file=paste(dirnew_data,"/population_times_series_all_homo.txt",sep=""))
-    outname=paste(dirnew_plots,"/population_time_series_all_homo.png",sep="")
-    pop.plot<-
-      ggplot(data = pop.time.series.all.homo) + geom_step(aes(x = time.step, y = prob.detect, color = length)) +
-      labs(x = "time step", y = "Probability of Detection") + theme_minimal()+ theme(text = element_text(size = 16))+
-      scale_y_continuous(limits = c(0,  max(pop.time.series.all.homo$prob.detect)+1))
-    ggsave(outname,plot=pop.plot)
+      scale_y_continuous(limits = c(0, max(df$prob.detect)+1))
+    ggsave(outname, plot=pop.plot)
+
   }
-  
-  
-  write.table(pop.time.series.lys.zip, file=paste(dirnew_data,"/population_time_series_lys2_zip.txt",sep=""))
-  outname=paste(dirnew_plots,"/population_time_series_lys2_zip.png",sep="")
-  pop.plot<-
-    ggplot(data = pop.time.series.lys.zip) + geom_step(aes(x = time.step, y = prob.detect, color = length)) +
-    labs(x = "time step", y = "Probability of Detection") + theme_minimal()+ theme(text = element_text(size = 16))+
-    scale_y_continuous(limits = c(0, max(pop.time.series.lys.zip$prob.detect)+1))
-  ggsave(outname,plot=pop.plot)
-  
-  
-  write.table(pop.time.series.lys.homo, file=paste(dirnew_data,"/population_times_eries_lys2_homo.txt",sep=""))
-  outname=paste(dirnew_plots,"/population_time_series_lys2_homo.png",sep="")
-  pop.plot<-
-    ggplot(data = pop.time.series.lys.homo) + geom_step(aes(x = time.step, y = prob.detect, color = length)) +
-    labs(x = "time step", y = "Probability of Detection") + theme_minimal()+ theme(text = element_text(size = 16))+
-    scale_y_continuous(limits = c(0,  max(pop.time.series.lys.homo$prob.detect)+1))
-  ggsave(outname,plot=pop.plot)
 }
-
-
 #########################################################################################################
 #########################################################################################################
 
-stats.plots <- function(dirnew_plots, lys.occupancy.firsts, stats.zipping){
+stats.plots <- function(dirnew_plots, lys.occupancy.firsts){
   
   final.firsts = as.data.frame(matrix(-1,test.replicates,3))
   names(final.firsts) = c("500","1000","2000")
@@ -133,26 +110,6 @@ stats.plots <- function(dirnew_plots, lys.occupancy.firsts, stats.zipping){
     geom_boxplot(outlier.colour ="red", position = position_dodge(1)) +
     stat_summary(fun = mean, geom = "point", shape = 8, size = 4)
   ggsave(file,plot=first.boxplot)
-  
-  
-  file = paste(dirnew_plots,"/first_zip_boxplot.png",sep="")
-  first.zip.boxplot <-
-    ggplot(stats.zipping[c(which(stats.zipping$first.zip != -1)),],
-           aes(x=length, y=first.zip, color=length)) +
-    geom_boxplot(fill = "white", position = position_dodge(1), size = 0.5) +
-    stat_summary(fun = mean, geom = "point", shape = 8, size = 3)+
-    ggtitle("Time step of first zipped macrohomology for each fragment")
-  ggsave(file,plot=first.zip.boxplot)
-  
-  
-  file = paste(dirnew_plots,"/half_detection_boxplot.png",sep="")
-  first.zip.boxplot <-
-    ggplot(stats.zipping[c(which(stats.zipping$half.detect != -1)),],
-           aes(x=length, y=half.detect, color=length)) +
-    geom_boxplot(fill = "white", position = position_dodge(1), size = 0.5) +
-    stat_summary(fun = mean, geom = "point", shape = 8, size = 3) +
-    ggtitle("Time step of half detection for each invading fragment")
-  ggsave(file,plot=first.zip.boxplot)
   
 }
 
