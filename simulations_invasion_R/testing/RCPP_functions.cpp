90934bc9a5d7a9e50194ba56e94915d29e3e39f9
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-05-28 16:30:58 +0200

    reimplementation de la fonction find.occupancy" en c++ pour gagner du temps

---
+++ simulations_invasion_R/testing/RCPP_functions.cpp
@@ -0,0 +1,65 @@
+//RCPP_functions.cpp
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+// [[Rcpp::export]]
+IntegerVector find_occupancies_remastered(const IntegerVector occupiedRAD51, const IntegerVector additional_removals, 
+                                          const int lower_window = 0,  const int upper_window = 0, const int n = 2069){
+  IntegerVector indices ;
+  IntegerVector remove (n);
+  int x1;
+  int x2;
+  int i;
+  int j;
+  
+  for (i = 0; i < occupiedRAD51.size(); i++){
+    x1 = occupiedRAD51[i]-7;
+    x2 = occupiedRAD51[i]+7;
+    if(x1 < 1){x1 = 1;}
+    for(j = x1; j<= x2; j++){
+      remove[j] = j;
+    }
+  }
+  
+  if(additional_removals[0]!=0){
+    for (i = 0; i < additional_removals.size(); i++){
+      x1 = additional_removals[i]-7;
+      x2 = additional_removals[i]+7;
+      if(x1 < 1){x1 = 1;}
+      for(j = x1; j<= x2; j++){
+        remove[j] = j;
+      }
+    }
+  }
+  
+  if(lower_window >0){
+    for (i = 0; i<=lower_window; i++){
+      remove[i] = i  ;
+    }
+  }
+  
+  if(upper_window >0){
+    for (i = upper_window; i<remove.length(); i++){
+      remove[i] = i  ;
+    }
+  }
+  
+  //remove = remove[remove > 0];
+  
+  /*
+  if(remove.length() > 0){
+    for(i =0; i<remove.length();i++){
+      indices = indices[indices != remove[i]];
+    }
+  }
+  */
+  
+  for(i = 1; i<(n-7);i++){
+    if(remove[i] == 0){
+      indices.push_back(i);
+    }
+  }
+  
+  return(indices);
+}
